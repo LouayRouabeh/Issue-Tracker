6227871f523991d50c6eeedbb1aa7da8ef437d2d
RedxWarrior <louayrouabeh46@gmail.com>
2024-01-28 12:57:54 +0100

    feat: add Assignee selection

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,5 @@
 import prisma from "@/prisma/client";
-import { IssueSchema } from "@/app/validationSchema";
+import { patchIssueSchema } from "@/app/validationSchema";
 import { NextRequest, NextResponse } from "next/server";
 import authOptions from "@/app/auth/authOptions";
 import { getServerSession } from "next-auth";
@@ -15,10 +15,17 @@ export async function PATCH(
         if (!session)
             return NextResponse.json({}, {status: 401});
         const body = await request.json();
-        const validation = IssueSchema.safeParse(body)
+        const validation = patchIssueSchema.safeParse(body)
         if (!validation.success)
             return NextResponse.json(validation.error.format(),
             {status: 400});
+
+        const {assignedToUserId, title, description} = body;
+        if (assignedToUserId){
+            const user = await prisma.user.findUnique({ where: {id: assignedToUserId}})
+            if (!user) 
+                return NextResponse.json({error: 'Invalid user'}, {status: 400})
+        }    
 
         const issue = await prisma.issue.findUnique({
             where: {id: parseInt(params.id)}
@@ -27,8 +34,9 @@ export async function PATCH(
         const updatedIssue = await prisma.issue.update({
             where: {id: issue.id},
             data: {
-                title: body.title,
-                description: body.description
+                title,
+                description,
+                assignedToUserId
             }
         });
 
---
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { Skeleton } from "@/app/components";
+import { Issue, User } from "@prisma/client";
+import { Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
+
+const AssigneeSelect = ({ issue }: { issue: Issue }) => {
+  const { data: users, error, isLoading } = useUsers();
+
+  if (isLoading) return <Skeleton />;
+  if (error) return null;
+
+  const assignIssue = (userId: string) => {
+    axios
+      .patch("/api/issues/" + issue.id, {
+        assignToUserId: userId || null,
+      })
+      .catch(() => {
+        toast.error("Changes could not be saved.");
+      });
+  };
+
+  return (
+    <>
+      <Select.Root
+        defaultValue={issue.assignedToUserId || ""}
+        onValueChange={assignIssue}
+      >
+        <Select.Trigger placeholder="Assign..."></Select.Trigger>
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Suggestions</Select.Label>
+            <Select.Item value="5">Unassigned</Select.Item>
+            {users?.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster></Toaster>
+    </>
+  );
+};
+
+const useUsers = () =>
+  useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000, //60 secounds
+    retry: 3,
+  });
+
+export default AssigneeSelect;
